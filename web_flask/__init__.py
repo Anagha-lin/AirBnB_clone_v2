5bec891b7e0e74a626f631326fecbf794cbb4997
Anagha-lin <linusanagha37@gmail.com>
2024-03-24 19:17:17 +0000

    a

--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -1,6 +1,4 @@
-#webflask/__init__.py
+# web_flask/__init__.py
 
 """Initialization file for the Flask web application."""
-
-# Empty file - This file is required to treat the directory as a Python package.
 
